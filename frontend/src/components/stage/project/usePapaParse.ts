5d371fd65d058194fa24ea6eea99f4a11ff9f45f
cassielchoo <cassielz0607@gmail.com>
2023-11-23 22:39:51 +0800

    完成首页 多人协作

--- frontend/src/components/stage/project/usePapaParse.ts
+++
@@ -1,7 +0,0 @@
-import VuePapaParse from 'vue-papa-parse';
-
-export default function usePapaParse() {
-  function csvToJson() {
-    this.$papa.parse('name,address,city,state,zip'); // error, can't read parse of undefined
-  }
-}
